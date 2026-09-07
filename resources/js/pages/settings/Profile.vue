44a4d9d05d2a810244b1f0cd4983402656e43fb2
Matthijs <matthijsbedaf05@gmail.com>
2026-09-07 10:59:19 +0200

    remove unresolved route

--- resources/js/pages/settings/Profile.vue
+++ resources/js/pages/settings/Profile.vue
@@ -12,9 +12,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { edit } from '@/routes/profile';
-/* @chisel-email-verification */
-import { send } from '@/routes/verification';
-/* @end-chisel-email-verification */
 
 defineOptions({
     layout: {
